97072ea63973c1dee46c2e694a78d9ae57be0b2f
BabyDell <A.Java.Coder7@gmail.com>
2024-05-19 21:49:31 +0000

    Unused function in Map class deleted.

--- Classes/Map/Map.h
+++ Classes/Map/Map.h
@@ -8,25 +8,11 @@ class Map {
 public:
   Map();
 
-  void newMap(std::pair<int, int> playerCoordinates,
-              std::pair<int, int> coordMonster1,
-              std::pair<int, int> coordMonster2,
-              std::pair<int, int> coordMonster3,
-              std::pair<int, int> ladderCoordinates);
-
   char GetPosition(std::pair<int, int> coordinate);
   void SetCoordinate(std::pair<int, int> coordinate);
 
   void PrintMap();
 
 private:
-  std::pair<int, int> coordMonster1;
-  std::pair<int, int> coordMonster2;
-  std::pair<int, int> coordMosnter3;
-
-  std::pair<int, int> ladderCoordinates;
-
-  std::pair<int, int> playerCoordinates;
-
   vector<vector<char>> map;
 };
